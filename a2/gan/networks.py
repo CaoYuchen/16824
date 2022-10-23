8919a11aa911356df45913820c00ffbb7d655f30
Joshua Rain <1014054551@qq.com>
2022-10-23 17:53:12 -0400

    GAN bone code

--- a2/gan/networks.py
+++ a2/gan/networks.py
@@ -94,11 +94,30 @@ class ResBlockUp(jit.ScriptModule):
     def __init__(self, input_channels, kernel_size=3, n_filters=128):
         super(ResBlockUp, self).__init__()
 
+        self.layers = torch.nn.Sequential(
+            nn.BatchNorm2d(input_channels),
+            nn.ReLU(),
+            nn.Conv2d(input_channels,
+                      n_filters,
+                      kernel_size=kernel_size,
+                      stride=(1, 1),
+                      padding=(1, 1),
+                      bias=False),
+            nn.BatchNorm2d(n_filters),
+            nn.ReLU()
+        )
+
+        self.residual = UpSampleConv2D(n_filters, padding=1)
+        self.shortcut = UpSampleConv2D(input_channels, kernel_size=1)
+
     @jit.script_method
     def forward(self, x):
         # TODO 1.1: Forward through the layers and implement a residual connection.
         # Apply self.residual to the output of self.layers and apply self.shortcut to the original input.
-        pass
+        out = self.layers(x)
+        out = self.residual(out) + self.shortcut(x)
+
+        return out
 
 
 class ResBlockDown(jit.ScriptModule):
@@ -122,11 +141,27 @@ class ResBlockDown(jit.ScriptModule):
     def __init__(self, input_channels, kernel_size=3, n_filters=128):
         super(ResBlockDown, self).__init__()
 
+        self.layers = torch.nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(input_channels,
+                      n_filters,
+                      kernel_size=kernel_size,
+                      stride=(1, 1),
+                      padding=(1, 1)),
+            nn.ReLU()
+        )
+
+        self.residual = DownSampleConv2D(n_filters, padding=1)
+        self.shortcut = DownSampleConv2D(input_channels, kernel_size=1)
+
     @jit.script_method
     def forward(self, x):
         # TODO 1.1: Forward through the layers and implement a residual connection.
         # Apply self.residual to the output of self.layers and apply self.shortcut to the original input.
-        pass
+        out = self.layers(x)
+        out = self.residual(out) + self.shortcut(x)
+
+        return out
 
 
 class ResBlock(jit.ScriptModule):
@@ -145,11 +180,36 @@ class ResBlock(jit.ScriptModule):
     def __init__(self, input_channels, kernel_size=3, n_filters=128):
         super(ResBlock, self).__init__()
 
+        self.layers = torch.nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(input_channels,
+                      n_filters,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(n_filters,
+                      n_filters,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=(1, 1)),
+        )
+
     @jit.script_method
     def forward(self, x):
         # TODO 1.1: Forward the conv layers. Don't forget the residual connection!
-        pass
+        out = self.layers(x)
 
+        return out + x
+
+
+class Reshape(nn.Module):
+    def __init__(self, shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.reshape(self.shape)
 
 class Generator(jit.ScriptModule):
     # TODO 1.1: Impement Generator. Follow the architecture described below:
@@ -213,17 +273,36 @@ class Generator(jit.ScriptModule):
     def __init__(self, starting_image_size=4):
         super(Generator, self).__init__()
 
+        self.dense = nn.Linear(in_features=128, out_features=2048, bias=True)
+        self.layers = nn.Sequential(
+            Reshape((-1, 128, starting_image_size, starting_image_size)),
+            ResBlockUp(128),
+            ResBlockUp(128),
+            ResBlockUp(128),
+            nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+            nn.ReLU(),
+            nn.Conv2d(128, 3, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.Tanh(),
+        )
+
     @jit.script_method
     def forward_given_samples(self, z):
         # TODO 1.1: forward the generator assuming a set of samples z have been passed in.
         # Don't forget to re-shape the output of the dense layer into an image with the appropriate size!
-        pass
+        out = self.dense(z)
+        out = self.layers(out)
+
+        return out
 
     @jit.script_method
     def forward(self, n_samples: int = 1024):
         # TODO 1.1: Generate n_samples latents and forward through the network.
         # Make sure to cast the latents to type half (for compatibility with torch.cuda.amp.autocast)
-        pass
+        z = torch.normal(0., 1., size=(n_samples, 128)).to(torch.float16).cuda()
+        out = self.dense(z)
+        out = self.layers(out)
+
+        return out
 
 
 class Discriminator(jit.ScriptModule):
@@ -282,8 +361,20 @@ class Discriminator(jit.ScriptModule):
     def __init__(self):
         super(Discriminator, self).__init__()
 
+        self.layers = nn.Sequential(
+            ResBlockDown(3),
+            ResBlockDown(128),
+            ResBlock(128),
+            ResBlock(128),
+            nn.ReLU(),
+        )
+        self.dense = nn.Linear(in_features=128, out_features=1, bias=True)
+
     @jit.script_method
     def forward(self, x):
         # TODO 1.1: Forward the discriminator assuming a batch of images have been passed in.
         # Make sure to flatten the output of the convolutional layers and sum across the image dimensions before passing to the output layer!
-        pass
+        out = self.layers(x)
+        out = self.dense(torch.sum(out, (2, 3)).view(x.size(0), -1))
+
+        return out
